40d412c57c665508892b42e25bc5abb6525bdb03
Tanas10 <adespotovski09@gmail.com>
2024-12-28 19:25:47 +0100

    Domashna3
    Models
    Images
    Controllers

---
+++ Домашна 3/DASProject/src/test/java/mk/finki/ukim/mk/dasproject/DasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.mk.dasproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
